20095e531ef39890957d7bd5437074edf4d211ba
Irina2294 <islivinscaia@gmail.com>
2024-09-19 06:54:39 -0500

    4th Commit

--- src/main/java/SeleniumHW2WebDriverCommandsAssign.java
+++ src/main/java/SeleniumHW2WebDriverCommandsAssign.java
@@ -3,15 +3,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumHW2WebDriverCommandsAssign {
     public static void main(String[] args) {
+      //  1. The user successfully opens Chrome and maximizes the window using `window().maximize()`.
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
+      // 2. The user navigates to `www.google.com`, and the page title is printed correctly.
         driver.navigate().to("https://www.google.com");
         String title=driver.getTitle();
         System.out.println("The title for google page is: "+ title);
+      // 3. The user navigates to `https://www.syntaxprojects.com/`, and the page title is printed correctly.
         driver.navigate().to("https://www.syntaxprojects.com/");
         String SyntaxTitle=driver.getTitle();
         System.out.println("Syntaxprojects title is: "+SyntaxTitle);
+      // 4. The user successfully navigates back to `www.google.com` using `navigate().back()`.
         driver.navigate().back();
+       //5. The page at `www.google.com` is refreshed using `navigate().refresh()`.
+        driver.navigate().refresh();
+       // 6. The browser closes after completing all the navigation steps.
         driver.quit();
 
 
